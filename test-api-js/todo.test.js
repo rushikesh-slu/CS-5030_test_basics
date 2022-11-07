22632848ec4c44c322776f7f1e679bd545c00d69
rushikesh-slu <rushikesh.nalamothu@slu.edu>
2022-11-06 21:41:23 -0600

    First test case

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -19,6 +19,12 @@ describe('todo test suite', () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
 
+
+    test("add_todo",()=>{
+        let c = todo_service.get_todos().todo.length
+        todo_service.add_todo({ "id": 4, "title": "Learn Java", "completed": false })
+        expect(todo_service.get_todos().todo.length).toEqual(c+1);
+    })
     
 
     // Write all your test cases here that corresponds to software requirements
